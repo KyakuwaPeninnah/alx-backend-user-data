77dc3ea3277206dadc93521aac1e596c1907486c
KyakuwaPeninnah <pennykyakuwa@gmail.com>
2024-11-07 18:52:12 +0000

    added file for encryption

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,8 +9,8 @@ def hash_password(password: str) -> bytes:
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-i
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """Checks if hashed password was formed from a given password.
+    """Checks is hashed password was formed from the given password.
     """
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
